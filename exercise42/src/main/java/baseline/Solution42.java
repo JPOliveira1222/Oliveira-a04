7c14514329812297bc5f6c002a24771769f8588d
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-17 21:17:48 -0400

    Pseudocode for exercise 42

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 42 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package baseline;
+
+public class Solution42 {
+
+/*
+    Create method that prints details from file
+    takes name of the file name
+
+    create a scanner that scans the file
+
+    prints out the contents of the file using a while loop
+
+    using an exception, catch if the file name is not found
+
+    print out the final list of the names in tabular form.
+     */
+
+}
