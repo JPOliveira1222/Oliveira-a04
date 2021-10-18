e7e28f55adefaab4658e7d9d5e1e022c39b4d9eb
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-17 21:39:12 -0400

    final product of exercise 42, cleaned up logic added comments

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -2,22 +2,52 @@
  *  UCF COP3330 Fall 2021 Assignment 42 Solution
  *  Copyright 2021 Joao Pedro Oliveira
  */
-
 package baseline;
 
+import java.util.*;
+import java.io.*;
+
 public class Solution42 {
+    
+    public static void main(String[] args) {
 
-/*
-    Create method that prints details from file
-    takes name of the file name
-
-    create a scanner that scans the file
-
-    prints out the contents of the file using a while loop
-
-    using an exception, catch if the file name is not found
-
-    print out the final list of the names in tabular form.
-     */
-
+        //prints out employee information from input file
+        Employee emp = new Employee();
+        System.out.printf("%-17s%-17s%-17s\n", "Last", "First", "Salary");
+        System.out.println("----------------------------------------");
+        emp.printDetails("exercise42_input.txt");
+    }
 }
+
+    class Employee {
+
+        //Method to print details
+        public void printDetails(String filename) {
+            try {
+
+                File file = new File(filename);
+
+                //scan file
+                Scanner scan = new Scanner(file);
+
+                //read out file details as long as file has next line
+                while (scan.hasNext()) {
+                    String s = scan.nextLine();
+                    String[] temp = s.split(",");
+
+                    //print out with spaces
+                    System.out.printf("%-17s%-17s%-17s\n", temp[0], temp[1], temp[2]);
+                }
+
+
+                //exception in case file is not found
+            } catch (FileNotFoundException ex) {
+                System.out.println("Error: File Not Found!!");
+            }
+        }
+    }
+
+
+
+
+
