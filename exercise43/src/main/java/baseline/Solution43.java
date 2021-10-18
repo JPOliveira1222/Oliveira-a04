6a1088faa806a2239289996c6627de01f6c20448
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-17 21:42:01 -0400

    Pseudocode for exercise 43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,47 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 43 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+package baseline;
+
+public class Solution43 {
+
+    /*
+
+     create 2 variables for javascript and css
+    js = N
+    css = N
+
+    Create a new scanner
+    print "Site name: "
+    siteName = user input
+
+    print "Author: "
+    author = user input
+
+    print "Do you want a folder for JavaScript? "
+    scan user input for js
+    if user input matches variables, do not create folders
+
+    scan user input for css
+    if user input matches variables, do not create folders
+
+    create a path using a directory = currentPath
+
+    if a website is created, then print "Created " + currentPath
+
+    if user input for creating a javascript and css folder = Y || y
+
+    create file paths using the directory
+
+    if not print "Js folder not created" || "CSS folder not created"
+
+    use exception to catch errors.
+    Print "Index.html not created as required"
+    or "Website folder not created"
+
+    close
+
+     */
+
+}
